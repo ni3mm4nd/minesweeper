5a4d5076f75d857066a2b2b46d9d9891b6b77081
Jan Kadlec <kadlec-jan@email.cz>
2023-09-05 15:49:38 +0200

    Refactoring

---
+++ internal/game/game.go
@@ -0,0 +1,213 @@
+package game
+
+import "math/rand"
+
+var GamePtr *gameStruct
+
+type gameStruct struct {
+	UserBoard     [][]int
+	RealBoard     [][]int
+	Height        int
+	Width         int
+	NumberOfMines int
+	Opened        int
+	TotalFields   int
+	IsGameOver    bool
+	IsWon         bool
+	IsLost        bool
+	Remaining     func() int
+}
+
+func NewGame(height int, width int, numberOfMines int) *gameStruct {
+	GamePtr = &gameStruct{
+		UserBoard:     [][]int{},
+		RealBoard:     [][]int{},
+		Height:        height,
+		Width:         width,
+		NumberOfMines: numberOfMines,
+		Opened:        0,
+		TotalFields:   height * width,
+		IsGameOver:    false,
+		IsWon:         false,
+		IsLost:        false,
+		Remaining:     func() int { return GamePtr.TotalFields - GamePtr.Opened - GamePtr.NumberOfMines },
+	}
+
+	GamePtr.TotalFields = height * width
+	GamePtr.RealBoard = createBoard(height, width)
+	fillWithMines(GamePtr.RealBoard, numberOfMines)
+	enrichBoard(GamePtr.RealBoard)
+	GamePtr.UserBoard = createBoard(height, width)
+
+	return GamePtr
+}
+
+func (g *gameStruct) ClickField(row int, col int) {
+	if g.UserBoard[row][col] == -3 || g.UserBoard[row][col] > 0 {
+		// fmt.Println("You can not click on already uncovered field!")
+		return
+	}
+
+	g.Opened++
+	if g.RealBoard[row][col] == -1 {
+		g.IsGameOver = true
+		g.IsLost = true
+		return
+	}
+
+	if g.RealBoard[row][col] == 0 {
+		g.UserBoard[row][col] = -3
+
+		//Find surrounding fields and if it's not a mine then click it
+		// Look at N up
+		if row > 0 {
+			if g.RealBoard[row-1][col] != -1 {
+				g.ClickField(row-1, col)
+			}
+		}
+
+		// Look at NE up right
+		if row > 0 && col < g.Width-1 {
+			if g.RealBoard[row-1][col+1] != -1 {
+				g.ClickField(row-1, col+1)
+			}
+		}
+
+		// Look at E right
+		if col < g.Width-1 {
+			if g.RealBoard[row][col+1] != -1 {
+				g.ClickField(row, col+1)
+			}
+		}
+
+		// Look at SE right down
+		if row < g.Height-1 && col < g.Width-1 {
+			if g.RealBoard[row+1][col+1] != -1 {
+				g.ClickField(row+1, col+1)
+			}
+		}
+
+		// Look at S down
+		if row < g.Height-1 {
+			if g.RealBoard[row+1][col] != -1 {
+				g.ClickField(row+1, col)
+			}
+		}
+
+		// Look at SW left down
+		if row < g.Height-1 && col > 0 {
+			if g.RealBoard[row+1][col-1] != -1 {
+				g.ClickField(row+1, col-1)
+			}
+		}
+
+		// Look at W left
+		if col > 0 {
+			if g.RealBoard[row][col-1] != -1 {
+				g.ClickField(row, col-1)
+			}
+		}
+
+		// Look at NW left up
+		if row > 0 && col > 0 {
+			if g.RealBoard[row-1][col-1] != -1 {
+				g.ClickField(row-1, col-1)
+			}
+		}
+
+		return
+	}
+
+	g.UserBoard[row][col] = g.RealBoard[row][col]
+
+	if g.TotalFields == (g.NumberOfMines + g.Opened) {
+		g.IsGameOver = true
+		g.IsWon = true
+		return
+	}
+}
+
+func enrichBoard(board [][]int) {
+	for i := 0; i < len(board); i++ {
+		for j := 0; j < len(board[0]); j++ {
+			if board[i][j] == -1 {
+
+				// Look at N up
+				if i > 0 {
+					if board[i-1][j] != -1 {
+						board[i-1][j]++
+					}
+				}
+
+				// Look at NE up right
+				if i > 0 && j < len(board[0])-1 {
+					if board[i-1][j+1] != -1 {
+						board[i-1][j+1]++
+					}
+				}
+
+				// Look at E right
+				if j < len(board[0])-1 {
+					if board[i][j+1] != -1 {
+						board[i][j+1]++
+					}
+				}
+
+				// Look at SE right down
+				if i < len(board)-1 && j < len(board[0])-1 {
+					if board[i+1][j+1] != -1 {
+						board[i+1][j+1]++
+					}
+				}
+
+				// Look at S down
+				if i < len(board)-1 {
+					if board[i+1][j] != -1 {
+						board[i+1][j]++
+					}
+				}
+
+				// Look at SW left down
+				if i < len(board)-1 && j > 0 {
+					if board[i+1][j-1] != -1 {
+						board[i+1][j-1]++
+					}
+				}
+
+				// Look at W left
+				if j > 0 {
+					if board[i][j-1] != -1 {
+						board[i][j-1]++
+					}
+				}
+
+				// Look at NW left up
+				if i > 0 && j > 0 {
+					if board[i-1][j-1] != -1 {
+						board[i-1][j-1]++
+					}
+				}
+			}
+		}
+	}
+}
+
+func createBoard(height int, width int) [][]int {
+	board := make([][]int, height)
+	for i := 0; i < height; i++ {
+		board[i] = make([]int, width)
+	}
+	return board
+}
+
+func fillWithMines(board [][]int, numberOfMines int) {
+	for i := 0; i < numberOfMines; i++ {
+		for {
+			var field int = board[rand.Intn(len(board))][rand.Intn(len(board[0]))]
+			if field != -1 {
+				board[rand.Intn(len(board))][rand.Intn(len(board[0]))] = -1
+				break
+			}
+		}
+	}
+}
--- main.go
+++ main.go
@@ -2,89 +2,41 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
 	"os"
+
+	"samoseto.com/minesweeper/internal/game"
 )
 
-// Change game parameters here
-const height int = 10
-const width int = 10
-const numberOfMines int = 10
-
-//
-
-var opened int = 0
-var totalFields int = 0
-
 func main() {
-	var realBoard [][]int = createBoard(height, width)
-	fillWithMines(realBoard, numberOfMines)
-	// printBoard(realBoard)
-	enrichBoard(realBoard)
-	var userBoard [][]int = createBoard(height, width)
-	// println()
-	// printBoard(realBoard)
-	fmt.Println("User board:")
-	printUserBoard(userBoard)
+	game := game.NewGame(10, 10, 10)
+	printBoard(game.UserBoard)
 
 	for {
-		fmt.Printf("Remaining fields: %d\n", totalFields-opened-numberOfMines)
+		fmt.Printf("Remaining fields: %d\n", game.Remaining())
 		var selectedRow int
 		var selectedCol int
 		fmt.Println("Select a row: ")
 		_, _ = fmt.Scanf("%d", &selectedRow)
 		fmt.Println("Select a column: ")
 		_, _ = fmt.Scanf("%d", &selectedCol)
-		clickField(realBoard, userBoard, selectedRow, selectedCol)
-		printUserBoard(userBoard)
-		if totalFields == (numberOfMines + opened) {
-			fmt.Println("You won!")
+		game.ClickField(selectedRow, selectedCol)
+		printBoard(game.UserBoard)
+		if game.IsGameOver {
+			printBoard(game.RealBoard)
+			fmt.Println("Game over!")
+
+			if game.IsWon {
+				fmt.Println("You won!")
+			} else if game.IsLost {
+				fmt.Println("You lost!")
+			}
+
 			os.Exit(0)
 		}
 	}
 }
 
-func createBoard(height int, width int) [][]int {
-	totalFields = height * width
-	board := make([][]int, height)
-	for i := 0; i < height; i++ {
-		board[i] = make([]int, width)
-	}
-	return board
-}
-
-func fillWithMines(board [][]int, numberOfMines int) {
-	for i := 0; i < numberOfMines; i++ {
-		for {
-			var field int = board[rand.Intn(len(board))][rand.Intn(len(board[0]))]
-			if field != -1 {
-				board[rand.Intn(len(board))][rand.Intn(len(board[0]))] = -1
-				break
-			}
-		}
-	}
-}
-
 func printBoard(board [][]int) {
-	fmt.Printf("  ")
-	for i := 0; i < len(board); i++ {
-		fmt.Printf("%d ", i)
-	}
-	fmt.Println()
-	for i := 0; i < len(board); i++ {
-		fmt.Printf("%d ", i)
-		for j := 0; j < len(board[0]); j++ {
-			if board[i][j] == -1 {
-				fmt.Printf("X ")
-			} else {
-				fmt.Printf("%d ", board[i][j])
-			}
-		}
-		println()
-	}
-}
-
-func printUserBoard(board [][]int) {
 	fmt.Printf("  ")
 	for i := 0; i < len(board); i++ {
 		fmt.Printf("%d ", i)
@@ -103,148 +55,4 @@ func printUserBoard(board [][]int) {
 		}
 		println()
 	}
-}
-
-func enrichBoard(board [][]int) {
-	// find field with value -1 and increment by 1 each surrounding field
-	for i := 0; i < len(board); i++ {
-		for j := 0; j < len(board[0]); j++ {
-			if board[i][j] == -1 {
-
-				// Look at N up
-				if i > 0 {
-					if board[i-1][j] != -1 {
-						board[i-1][j]++
-					}
-				}
-
-				// Look at NE up right
-				if i > 0 && j < len(board[0])-1 {
-					if board[i-1][j+1] != -1 {
-						board[i-1][j+1]++
-					}
-				}
-
-				// Look at E right
-				if j < len(board[0])-1 {
-					if board[i][j+1] != -1 {
-						board[i][j+1]++
-					}
-				}
-
-				// Look at SE right down
-				if i < len(board)-1 && j < len(board[0])-1 {
-					if board[i+1][j+1] != -1 {
-						board[i+1][j+1]++
-					}
-				}
-
-				// Look at S down
-				if i < len(board)-1 {
-					if board[i+1][j] != -1 {
-						board[i+1][j]++
-					}
-				}
-
-				// Look at SW left down
-				if i < len(board)-1 && j > 0 {
-					if board[i+1][j-1] != -1 {
-						board[i+1][j-1]++
-					}
-				}
-
-				// Look at W left
-				if j > 0 {
-					if board[i][j-1] != -1 {
-						board[i][j-1]++
-					}
-				}
-
-				// Look at NW left up
-				if i > 0 && j > 0 {
-					if board[i-1][j-1] != -1 {
-						board[i-1][j-1]++
-					}
-				}
-			}
-		}
-	}
-}
-
-func clickField(realBoard [][]int, userBoard [][]int, row int, col int) {
-	if userBoard[row][col] == -3 || userBoard[row][col] > 0 {
-		// fmt.Println("You can not click on already uncovered field!")
-		return
-	}
-
-	opened++
-	if realBoard[row][col] == -1 {
-		fmt.Println("BOOOOOOOOM!")
-		printBoard(realBoard)
-		os.Exit(0)
-	}
-
-	if realBoard[row][col] == 0 {
-		userBoard[row][col] = -3
-
-		//Find surrounding fields and if it's not a mine then click it
-		// Look at N up
-		if row > 0 {
-			if realBoard[row-1][col] != -1 {
-				clickField(realBoard, userBoard, row-1, col)
-			}
-		}
-
-		// Look at NE up right
-		if row > 0 && col < len(realBoard[0])-1 {
-			if realBoard[row-1][col+1] != -1 {
-				clickField(realBoard, userBoard, row-1, col+1)
-			}
-		}
-
-		// Look at E right
-		if col < len(realBoard[0])-1 {
-			if realBoard[row][col+1] != -1 {
-				clickField(realBoard, userBoard, row, col+1)
-			}
-		}
-
-		// Look at SE right down
-		if row < len(realBoard)-1 && col < len(realBoard[0])-1 {
-			if realBoard[row+1][col+1] != -1 {
-				clickField(realBoard, userBoard, row+1, col+1)
-			}
-		}
-
-		// Look at S down
-		if row < len(realBoard)-1 {
-			if realBoard[row+1][col] != -1 {
-				clickField(realBoard, userBoard, row+1, col)
-			}
-		}
-
-		// Look at SW left down
-		if row < len(realBoard)-1 && col > 0 {
-			if realBoard[row+1][col-1] != -1 {
-				clickField(realBoard, userBoard, row+1, col-1)
-			}
-		}
-
-		// Look at W left
-		if col > 0 {
-			if realBoard[row][col-1] != -1 {
-				clickField(realBoard, userBoard, row, col-1)
-			}
-		}
-
-		// Look at NW left up
-		if row > 0 && col > 0 {
-			if realBoard[row-1][col-1] != -1 {
-				clickField(realBoard, userBoard, row-1, col-1)
-			}
-		}
-		return
-	}
-
-	userBoard[row][col] = realBoard[row][col]
 }
